a3d77d558d2fa09dbb56554609d1e010dad55bc0
liangchi <liangchi@liangchideMacBook-Pro.local>
2018-03-08 12:04:32 +0800

    test.py

--- xgboost_training/test.py
+++ xgboost_training/test.py
@@ -1,11 +1,11 @@
-#Import libraries:
+Import libraries:
 import pandas as pd
 import numpy as np
 import xgboost as xgb
 from xgboost.sklearn import XGBClassifier
 from sklearn import cross_validation, metrics   #Additional     scklearn functions
 from sklearn.grid_search import GridSearchCV   #Perforing grid search
-
+import os
 import matplotlib.pylab as plt
 # %matplotlib inline
 from matplotlib.pylab import rcParams
